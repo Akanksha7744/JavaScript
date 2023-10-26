62fdc54c431fc7d1b15b7b8160c6231782f1a57a
Akanksha Gurunath Halle <113449675+Akanksha7744@users.noreply.github.com>
2023-10-26 23:44:35 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,56 @@
+console.log("Operators in JS");
+//Arithmatic Op
+let a = 80;
+let b = 20;
+console.log("a+b=", a + b);
+console.log("a-b=", a - b);
+console.log("a*b=", a * b);
+console.log("a/b=", a / b);
+console.log("a%b=", a % b);
+let c = 20;
+let d = 2;
+console.log("c**d=", c ** d);
+
+//Increament And Decrement Operator
+console.log("Increament And Decrement Operator");
+console.log("c++=", c++);
+console.log("++c=", ++c);
+console.log("c--=", c--);
+console.log("--c=", --c);
+
+//Assignment Operators
+console.log("Assignment Operators");
+a = 8;
+console.log(a);
+a += 5;
+console.log(a);
+a -= 3;
+console.log(a);
+a *= 2;
+console.log(a);
+
+//Comparision Operators
+console.log("Comparision Operators");
+let comp1 = 10;
+let comp2 = 20;
+let comp3 = 20;
+console.log("comp1==com2", comp1 == comp2);
+console.log("comp2==com3", comp2 == comp3);
+console.log("comp1<com2", comp1 < comp2);
+console.log("comp1>com2", comp1 > comp2);
+console.log("comp1!=com2", comp1 != comp2);
+
+console.log("triple ===");
+let l1 = 24;
+let l2 = "24";
+console.log("l1===l2", l1 === l2);
+console.log("l1==l2", l1 == l2);
+
+//Logical Operators
+
+console.log("Logical Operators");
+let x = 5;
+let y = 6;
+console.log(x < y && x == 5);
+console.log(x < y || x == 6);
+console.log(x < y != x == 5);
