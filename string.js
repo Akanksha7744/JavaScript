fa4b4b1a7420bce2aba28c947ba6f1d106b7beab
Akanksha Gurunath Halle <113449675+Akanksha7744@users.noreply.github.com>
2023-10-27 10:49:26 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,27 @@
+/*let name='Akank\'sha';
+console.log(name);
+let fruit="Banana\"na";
+console.log(fruit);*/
+
+//String Methods
+/*let name="Surekha";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2));
+console.log(name.replace("Sur","Tur"));
+*/
+let n1 = "Gurunath";
+let n2 = "Halle";
+console.log(n1.concat(" surname is " + n2));
+
+let friend = "     meena      ";
+console.log(friend.trim());
+
+let fr = "Shivika" + "Harry";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+console.log(fr);
